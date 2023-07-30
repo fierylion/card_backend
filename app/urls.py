d96893d2f31f8f886b3f72ff0779b0fbaf7b4bbc
fierylion <mawalladaniel2018@gmail.com>
2023-07-30 21:50:56 +0300

    add: payment options

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, PaymentOperationView
 app_urls = [
     path('user/register', UserView.as_view(
         {
-            'post':'create_single_user'
+            'post': 'create_single_user'
         }
-    ))
+    )),
+    path('user/payment/generate', PaymentOperationView.as_view(
+        {
+            'post': 'create_payment_link'
+        }
+    )
+    ),
+    path('user/payment/callback', PaymentOperationView.as_view(
+        {
+            'post': 'receive_callback'
+        }
+    )),
 ]
--- app/views.py
+++ app/views.py
@@ -13,4 +13,9 @@ class UserView(ViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PaymentOperationView(ViewSet):
+    def create_payment_link(self, request):
+        pass
+    def receive_callback(self, request):
+        pass
   
