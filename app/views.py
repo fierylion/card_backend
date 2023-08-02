c78d9c217c6d9ffde1ce284a6e8eaa1063645575
fierylion <mawalladaniel2018@gmail.com>
2023-08-02 17:32:38 +0300

    first commit

--- app/views.py
+++ app/views.py
@@ -102,7 +102,7 @@ class PaymentOperationView(ViewSet):
         request.data.pop("user")
         user_required = CustomUser.objects.filter(reference=request.data['utilityref']).first()
        
-        # request.data['user']=user_required.id
+        request.data['user']=user_required.id
         serializer=TransactionSerializer(data=request.data)
         if serializer.is_valid():
             print("valid")
